72e0f2e9c196ccef7ea812e84aedbca7b154ca63
Ivan Zamora <ivzamhdz@gmail.com>
2023-10-02 11:32:15 -0600

    cleaned poms and fixed serialization issue in Orde model

--- common/src/main/java/com/ivansoft/java/dapr/emarket/common/models/Order.java
+++ common/src/main/java/com/ivansoft/java/dapr/emarket/common/models/Order.java
@@ -1,25 +1,31 @@
 package com.ivansoft.java.dapr.emarket.common.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NonNull;
-import lombok.ToString;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
-@ToString
 public class Order implements Serializable {
+    private static final long serialVersionUID = 123456789L;
+
     @NonNull
+    @JsonProperty("client")
     String client;
 
     @NonNull
+    @JsonProperty("product")
     String product;
 
+    @JsonProperty("quantity")
     int quantity;
 
+    @JsonProperty("status")
     String status;
 
+    @JsonProperty("price")
     double price;
 }
